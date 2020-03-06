92f0ca7710f8027b68a88859054f17262e4dadfd
GAlemsky <defake@yandex.ru>
2020-03-07 00:17:18 +0500

    Add use of environment variables

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
+const dotenv = require('dotenv');
 const express = require('express');
 
 const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+dotenv.config();
 
 require('./config/server-config')(app, express);
 require('./api/routes')(app);
 
-app.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at http://${process.env.HOSTNAME}:${process.env.PORT}/`);
 });
