c168168f37bad00b775ce38e41d79573978826e3
GeneralShtorks <generalshtorks@gmail.com>
2020-04-01 15:47:02 +0500

    Fixed repeat in router

--- src/api/routes.js
+++ src/api/routes.js
@@ -17,10 +17,6 @@ router.get('/', (req, res) => {
   res.render('index', { tasks: db.getTasks() });
 });
 
-router.get('/', (req, res) => {
-  res.render('index', { tasks: db.getTasks() });
-});
-
 router.get('/about', (req, res) => {
   res.render('about');
 });
