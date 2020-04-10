b2e4aaf4efe7642f38ded60c7955451f9dd3500c
GAlemsky <defake@yandex.ru>
2020-04-10 10:06:51 +0500

    Configure proper request logging. Move it to server-config

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -1,9 +1,21 @@
 const bodyParser = require('body-parser');
 
-module.exports = function (app, express) {
+function configureLogging(app) {
+  if (process.env.NODE_ENV === 'dev') {
+    app.use((req, res, next) => {
+      console.info(`[${new Date().toISOString()}]: ${req.method} ${req.originalUrl}\n`
+        + `Headers: ${JSON.stringify(req.headers, null, 2)} \n`
+        + `Body: ${JSON.stringify(req.body, null, 2)}`);
+      next();
+    });
+  }
+}
+
+module.exports = (app, express) => {
   app.set('views', `${__dirname}/../../public/templates`);
   app.set('view engine', 'ejs');
   app.use('/public', express.static(`${__dirname}/../../public`));
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
+  configureLogging(app);
 };
