d85fdf5bdb249d7109297dab4abdf0222fc83e20
GAlemsky <defake@yandex.ru>
2020-04-22 23:58:21 +0500

    Replace deprecated webpack extract plugin with newer analogue

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const extractPlugin = new ExtractTextPlugin({ filename: 'dist/main.css' });
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env, argv) => {
   const buildPath = 'build';
@@ -21,13 +18,13 @@ module.exports = (env, argv) => {
     },
     module: {
       rules: [
-        { test: /\.scss$/, use: extractPlugin.extract({ use: ['css-loader', 'sass-loader'] }) },
-        { test: /\.css$/, use: extractPlugin.extract({ use: ['css-loader'] }) },
+        { test: /\.scss$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] },
+        { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
         { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       ],
     },
     plugins: [
-      extractPlugin,
+      new MiniCssExtractPlugin({ filename: 'dist/main.css' }),
       new CopyWebpackPlugin([
         { from: 'public/templates', to: 'dist/templates' },
         { from: '.env', to: './' },
